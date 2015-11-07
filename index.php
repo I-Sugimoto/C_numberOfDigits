0249396d8ec4b7fdbb7335b254dc0f374d340127
Ai Sugimoto <i.sugimoto1112@gmail.com>
2015-11-08 00:47:44 +0900

    modified

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
-<php?
-echo hello
+<?php
+$array = array('123789', '100.5', 'sugimo-');
+
+foreach ($array as $n) 
+{
+  if (ctype_digit($n)) 
+    {   
+    	//10進数の場合。
+  		echo "この10進数の桁数は" . mb_strlen($n) . "桁です。" . "<br>";
+  	}
+  else
+    {   //10進数では無い場合。
+        echo "これは10進数ではありません。" . "<br>";
+    }	
+}
+
